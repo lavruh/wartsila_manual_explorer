15daa8c7a9f85ab620d7e081490fbae44e452dfa
lavruh <lavrsi87@gmail.com>
2025-01-08 12:32:29 +0200

    add search to rel doc view

--- lib/rel_doc_view.dart
+++ lib/rel_doc_view.dart
@@ -1,9 +1,12 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:pdfrx/pdfrx.dart';
 import 'package:path/path.dart' as p;
 import 'package:wartsila_manual_explorer/bookmark_ref.dart';
 import 'package:wartsila_manual_explorer/man_view.dart';
 import 'package:wartsila_manual_explorer/related_documents_list.dart';
+import 'package:wartsila_manual_explorer/utils/export_pdf.dart';
 
 class RelDocumentView extends StatefulWidget {
   const RelDocumentView({
@@ -39,7 +42,7 @@ class _RelDocumentViewState extends State<RelDocumentView> {
           Widget body = Container();
           String title = "Related documents:";
           Widget? leading;
-
+          List<Widget> actions = [];
 
           if (val == null) {
             body = RelatedDocumentsList(
@@ -49,18 +52,69 @@ class _RelDocumentViewState extends State<RelDocumentView> {
           } else {
             leading = IconButton(
               icon: const Icon(Icons.arrow_back),
-              onPressed: () => relatedDocViewPath.value = null,
+              onPressed: () {
+                textSearcher.resetTextSearch();
+                relatedDocViewPath.value = null;
+              },
             );
             title = p.basename(val);
-            // TODO: Add icon button to export pdf for printing
+            actions.add(SizedBox(
+              width: MediaQuery.of(context).size.width * 0.15,
+              child: TextField(
+                decoration: InputDecoration(
+                    labelText: "Search",
+                    suffix: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        IconButton(
+                          onPressed: (textSearcher.currentIndex ?? 0) <
+                                  textSearcher.matches.length
+                              ? () async {
+                                  await textSearcher.goToNextMatch();
+                                }
+                              : null,
+                          icon: const Icon(Icons.arrow_downward),
+                          iconSize: 20,
+                        ),
+                        IconButton(
+                          onPressed: (textSearcher.currentIndex ?? 0) <
+                                  textSearcher.matches.length
+                              ? () async {
+                                  await textSearcher.goToPrevMatch();
+                                }
+                              : null,
+                          icon: const Icon(Icons.arrow_upward),
+                          iconSize: 20,
+                        ),
+                      ],
+                    )),
+                onSubmitted: (val) {
+                  if (val.isNotEmpty) {
+                    textSearcher.startTextSearch(val);
+                  }
+                },
+              ),
+            ));
+            actions.add(
+              IconButton(
+                  onPressed: () {
+                    final export = File(val);
+                    exportPdf(originalManual: export, context: context);
+                  },
+                  icon: Icon(Icons.save_alt)),
+            );
             body = ManView(
                 manualPath: val,
-                controller: PdfViewerController(),
+                controller: controller,
                 textSearcher: textSearcher);
           }
 
           return Scaffold(
-            appBar: AppBar(title: Text(title), leading: leading),
+            appBar: AppBar(
+              title: Text(title),
+              leading: leading,
+              actions: actions,
+            ),
             body: body,
           );
         });
